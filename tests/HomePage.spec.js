0decc019db9c56b98e30ee290cd549549c8960ae
Aswin Syras <aswinsyras@gmail.com>
2026-02-24 18:11:35 +0530

    File name updated

---
+++ tests/HomePage.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://live.fwfg.gizmott.com/');
+  // Expect a title "to contain" a substring.
+  await page.waitForTimeout(3000);
+  await expect(page).toHaveTitle('Find What Feels Good');
+});
+
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//   //await page.waitForTimeout(3000);
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
