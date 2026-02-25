1ddaaf6deacf85dee36526db80623f93f9147f30
Aswin Syras <aswinsyras@gmail.com>
2026-02-25 12:58:59 +0530

    Added staging env variable

--- pages/Login.spec.js
+++ pages/Login.spec.js
@@ -2,5 +2,5 @@ import test from "node:test";
 
 export async function login (page) {
     await page.click('.signIn.black-text');
-    await page.waitForTimeout(5000);
+   // await page.waitForTimeout(5000);
 }
--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -3,18 +3,8 @@ import { test, expect } from '@playwright/test';
 import { login } from '../pages/Login.spec';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://live.fwfg.gizmott.com/');
-  // Expect a title "to contain" a substring.
-  await page.waitForTimeout(3000);
+  await page.goto('/');
   await expect(page).toHaveTitle('Find What Feels Good');
+  await page.waitForTimeout(3000);
   await login(page);
 });
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-//   //await page.waitForTimeout(3000);
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
