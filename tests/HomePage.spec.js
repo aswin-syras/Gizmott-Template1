fa43884863d2652b3e6fa09d78bca5482c9575d1
Aswin Syras <aswinsyras@gmail.com>
2026-02-26 14:59:45 +0530

    slider click

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../pages/Login.spec';
+import { disableTransitions } from '../helpers/disabletransition.spec.js';
 
-test('has title', async ({ page }) => {
+test('Navigate to FWFG Homepage', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle('Find What Feels Good');
   await page.waitForTimeout(5000);
-  await login(page);
+  //await login(page);
+  await page.click('.background');
+  await page.getByLabel('Banner Slide 4').getByRole('button', { name: 'Watch Now' }).click();
+  await expect(page).toHaveURL('https://live.fwfg.gizmott.com/show-details/unconditional-love')
+  await page.waitForTimeout(5000);
+
 });
