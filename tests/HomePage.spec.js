3b3ab3be6902738977c9ee953be3c426b3344bd6
Aswin Syras <aswinsyras@gmail.com>
2026-03-24 10:21:21 +0530

    Working code

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../pages/Login.spec';
+import { subscribedUser } from '../pages/Subscribed.spec.js';
 import { disableTransitions } from '../helpers/disabletransition.spec.js';
 
 test('Navigate to FWFG Homepage', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle('Find What Feels Good');
   await page.waitForTimeout(5000);
-  //await login(page);
-  await page.click('.background');
-  await page.getByLabel('Banner Slide 4').getByRole('button', { name: 'Watch Now' }).click();
-  await expect(page).toHaveURL('https://live.fwfg.gizmott.com/show-details/unconditional-love')
+  await login(page);
+  await subscribedUser(page);
+  // await page.click('.background');
+  // await page.getByLabel('Banner Slide 4').getByRole('button', { name: 'Watch Now' }).click();
+  // await expect(page).toHaveURL("https://live.fwfg.gizmott.com/show-details/kids-yoga-calm");
   //await page.waitForTimeout(5000);
 });
