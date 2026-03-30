65af5cdf0a75168f5b9466bf9a7cba4e1a93a0ad
Sreelakshmi308 <ssreelakshmi308@gmail.com>
2026-03-30 18:15:01 +0530

    changed code now working

--- testcases/Unsubscribed.spec.js
+++ testcases/Unsubscribed.spec.js
@@ -7,17 +7,20 @@ export async function unsubscribedUser(page) {
         // const username=page.locator('.light-email');
         // await username.fill('thomas@gmail.com');  
         // await page.locator('input[type=password]').fill("Temp@123");
-        
+
         // // Click Sign In
         // await page.getByRole('button', { name: 'Sign In', exact: true }).click();
         // await page.waitForURL('./'); 
         await login(page, "thomas@gmail.com", "Temp@123");
-        
+
         // Scroll and Click
-        await page.locator('.title').first().click();
-        await page.getByLabel('Show Main Info').getByText('Watch Now').click();
-        
-        
+        const firstVideo = page.locator('.showCardContainer').first();
+        await firstVideo.waitFor({ state: 'visible', timeout: 5000 });
+        await firstVideo.click();
+
+        //Click Watch Now
+        await page.getByRole('button',{name:'Watch Now',exact:true}).first().click();
+
         // Validate final URL
         await expect(page).toHaveURL('https://live.fwfg.gizmott.com/checkout', { timeout: 10000 });
 }
