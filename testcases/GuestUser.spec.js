ccd10455f8d21ed5242f7aa1c96243e57d2ef966
Sreelakshmi308 <ssreelakshmi308@gmail.com>
2026-04-16 17:58:21 +0530

    search functionality completed

--- testcases/GuestUser.spec.js
+++ testcases/GuestUser.spec.js
@@ -1,5 +1,6 @@
 import { expect } from "@playwright/test";
 import { loadPage } from "./PageLoad.spec";
+import { searchQuery } from "../helpers/SearchQuery.spec.js";    
 
 export async function guestUser(page){
     await loadPage(page);
@@ -11,5 +12,8 @@ export async function guestUser(page){
     await page.getByRole('button',{name:'Watch Now',exact:true}).first().click();
     //Validate final URL
     await expect(page).toHaveURL(`${process.env.BASE_URL}/login`,{timeout:10000});
-
+    await page.getByRole('link', { name: 'Home' }).click();
+    await page.waitForURL(`${process.env.BASE_URL}/`);
+    await searchQuery(page,'dsghdj');
 }
+
