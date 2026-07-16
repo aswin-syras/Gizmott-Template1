3b98196174415d98bef849e6539d182f39a3b3ab
Aswin Syras <aswinsyras@gmail.com>
2026-07-16 12:58:52 +0530

    Search query updated

--- testcases/SearchResult.spec.js
+++ testcases/SearchResult.spec.js
@@ -2,7 +2,7 @@ import { page, expect } from '@playwright/test'
 
 
 export async function search(page) {
-const keywords = ['Open and Release', 'Goodnight Sound Bath', 'Curvy-friendly Flow', 'Body Scan Meditation', 'Pilates for Desk Jobs', 'Workout with Aaron - Strong Back', 'What Lives on Repeat - March 2026 Members Vlog', 'Throat Chakra Sound Bath', 'Breathwork with Kris: Emotional Release', 'Breathing Room: 7-Video Series', 'Portraits of Practice: Marnie Castor', 'Kids Yoga: Calm', 'Flow - A 30 Day Yoga Journey', 'TEND - April 2026', 'Yoga For Beginners', 'Find Your Flow', 'hjkhkjhlk'];
+const keywords = ['Open and Release', 'Goodnight Sound Bath', 'Curvy-friendly Flow', 'Body Scan Meditation', 'Pilates for Desk Jobs', 'Workout with Aaron - Strong Back', 'What Lives on Repeat - March 2026 Members Vlog', 'Throat Chakra Sound Bath', 'Breathwork with Kris: Emotional Release', 'Breathing Room: 7-Video Series', 'Portraits of Practice: Marnie Castor', 'Kids Yoga: Calm', 'Flow - A 30 Day Yoga Journey', 'TEND - April 2026', 'Yoga For Beginners', 'Find Your Flow'];
 
 const patterns = [/Open and Release/i, /Goodnight Sound Bath/i, /Curvy.*/i, /Body Scan Meditation/i, /Pilates for Desk Jobs/i, /Workout with Aaron/i, /What Lives on Repeat/i, /Throat Chakra Sound Bath/i, /Breathwork with Kris/i, /Breathing Room/i, /Portraits of Practice/i, /Kids Yoga/i, /Flow - A 30 Day/i, /TEND - April/i, /Yoga For Beginners/i, /Find Your Flow/i, /hjkhkjhlk/i];
 
